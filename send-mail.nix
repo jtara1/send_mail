ed5f4fabd83a37b475a9ebd69a119ddc1494cf6a
James T <github.jtara1@gmail.com>
2024-10-11 18:44:23 -0700

    build: included meta in derivation

--- send-mail.nix
+++ send-mail.nix
@@ -5,10 +5,14 @@ with pkgs;
 stdenv.mkDerivation {
   pname = "send-mail";
   version = "1.0";
+  meta = {
+    description = "simple command-line for sending an email with remote auth";
+    maintainers = [ "jtara1" ];
+    license = lib.licenses.asl20;
+  };
+
   src = ./send_mail.py;
-
   buildInputs = [ python3 ];
-
   dontUnpack = true;
   postInstall = ''
     mkdir -p $out/bin
